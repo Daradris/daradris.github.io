5df97f348942e2d2a3a51d714797432fc03bd490
Sam <32861928+Daradris@users.noreply.github.com>
2021-06-29 20:17:55 +0200

    replace

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,7 +108,7 @@ Table of Content
 
     });
 
-    $("#menu-left").load("assets/html/menu.html");
+    $("#menu-left").replaceWith("assets/html/menu.html");
 
     // Document on Ready
     $(document).on("ready", function () {
